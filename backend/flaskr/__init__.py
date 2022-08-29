79d53a7d93b9f3148c775cbeae86a586ef9ddea1
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2022-08-29 16:53:59 +0100

    TODO Error handlers completed

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -250,5 +250,29 @@ def create_app(test_config=None):
     including 404 and 422.
     """
 
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 404, "message": "resource not found"}),
+            404,
+        )
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({"success": False, "error": 422, "message": "unprocessable"}),
+            422,
+        )
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({"success": False, "error": 400, "message": "bad request"}), 400
+
+
+    @app.errorhandler(500)
+    def bad_request(error):
+        return jsonify({"success": False, "error": 500, "message": "internal server error"}), 500
+
+
     return app
 
